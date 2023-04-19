ec3cd3052705d6c9d8ac61b5741ffbbb855933cb
aodemmon <aodemmon@umich.edu>
2023-04-18 23:40:22 -0400

    pushing

---
+++ js/professional.js
@@ -0,0 +1,9 @@
+document.getElementById("resume").addEventListener("click", function() {
+    window.open("dist/pdfs/!demmon_andrew_RESUME.pdf");
+});
+  
+document.getElementById("CL").addEventListener("click", function() {
+    window.open('dist/pdfs/!demmon_andrew_COVERLETTER.pdf');
+});
+
+  
